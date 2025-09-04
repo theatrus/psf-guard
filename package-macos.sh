5dd5759de879c7c5288d355f724a46bf00f8cae2
Yann Ramin <github@theatr.us>
2025-09-03 21:13:48 -0700

    Package

--- package-macos.sh
+++ package-macos.sh
@@ -2,7 +2,11 @@
 
 set -euo pipefail
 
-bash build-macos.sh tauri build --no-bundle
-bash scripts/macos-opencv-dylibs.sh
+if [ -f $HOME/.applekeys/use ]; then
+    source $HOME/.applekeys/use
+fi
+
+#bash build-macos.sh tauri build --no-bundle
+#bash scripts/macos-opencv-dylibs.sh
 bash build-macos.sh tauri bundle
 
